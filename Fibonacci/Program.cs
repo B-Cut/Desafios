38bd20046847d49905274316432bc675b51a4758
C. Gonçalves <cgoncalves@id.uff.br>
2024-09-21 14:48:41 -0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,93 @@
+﻿// Criamos um dicionário que irá conter as respostas já obtidas, evitando operações redundantes
+var answers = new Dictionary<int, int>(){
+    {0, 0},
+    {1, 1}
+};
+
+var running = true;
+
+const string positive = "Valor pertence à sequência de fibonacci";
+const string negative = "Valor não pertence à sequência de fibonacci";
+const string error = "Valor inválido";
+
+Console.WriteLine("***** FIBONACCI *****");
+Console.WriteLine("Para sair, insira x");
+
+while (running)
+{
+    Console.Write("Insira um número\n>");
+    string? input = Console.ReadLine();
+    if (input == null)
+    {
+        continue;
+    }
+    else if (input.Equals("x", StringComparison.OrdinalIgnoreCase))
+    {
+        running = false;
+    }
+    else
+    {
+        int value;
+        if (!int.TryParse(input, out value))
+        {
+            Console.WriteLine(error);
+        }
+        else if (value <= 0)
+        {
+            Console.WriteLine(negative);
+        }
+        else
+        {
+            if (valueIsInSequence(value))
+            {
+                Console.WriteLine(positive);
+            }
+            else
+            {
+                Console.WriteLine(negative);
+            }
+        }
+    }
+}
+
+int fibonacci(int index)
+{
+    if (answers.Keys.Contains(index))
+    {
+        return answers[index];
+    }
+
+    var results = fibonacci(index - 1) + fibonacci(index - 2);
+    answers[index] = results; // Guardamos o novo resultado no dicionário
+    return results;
+}
+
+bool valueIsInSequence(int value)
+{
+    if (value < 0)
+    {
+        return false;
+    }
+    if (answers.Values.Contains(value))
+    {
+        return true;
+    }
+
+    // Se o último valor é maior que o valor dado, não é possível que o número esteja na sequencia
+    if (answers.Values.Last() > value)
+    {
+        return false;
+    }
+
+    while (answers.Values.Last() < value)
+    {
+        // Sempre último valor processado + 1
+        var temp = fibonacci(answers.Count);
+        if (temp == value)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
---
+++ Verifica/Program.cs
@@ -0,0 +1,40 @@
+﻿// Acredito que esse programa funciona melhor como uma ferramenta da CLI
+// Mas pelo seu tamanho, não irei usar uma biblioteca especializada
+namespace Verifica
+{
+    internal class Program
+    {
+        static string helpText = @"Usage: verifica <string 1> <string 2> ...
+
+Essa ferramenta verifica quantos vezes o caractere 'a' aparece na string fornecida. Case insensitive";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** VERIFICA *****");
+            if (args.Length == 0 || args[0].Equals("-h") || args[0].Equals("--help"))
+            {
+                Console.WriteLine(helpText);
+                return;
+            }
+
+            foreach (string arg in args)
+            {
+                var numOfAs =
+                    from c in arg
+                    where (c == 'A' || c == 'a')
+                    select c;
+
+                var count = numOfAs.Count();
+                if (count == 0)
+                {
+                    Console.WriteLine($"A string {arg} não contém a letra 'a'");
+                }
+                else
+                {
+                    Console.WriteLine($"A string {arg} contém {numOfAs.Count()} ocorrências da letra 'a'");
+                }
+            }
+
+        }
+    }
+}
+
